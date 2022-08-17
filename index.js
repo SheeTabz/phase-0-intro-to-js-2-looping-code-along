074c4850ca85944e29667b75ff1a03faaa8d6e88
SheeTabz <wmuriithitabitha@gmail.com>
2022-08-17 13:11:40 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 // for (let age = 30; age < 40; age++) {
 //     console.log(`I'm ${age} years old. Happy birthday to me!`);
 //     // debugger;
-//   }
+  
   const gifts = ["teddy bear", "drone", "doll"];
 
 function wrapGifts(gifts) {
@@ -35,3 +35,31 @@ function countDown(num){
 
 }
 countDown(10)
+// const gifts = ["Teddy bear", "drone", "doll"]
+// function wrapGifts(gifts){
+//    for (let i=0; i<gifts.length; i++){
+//     console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//    }
+
+// }
+
+// wrapGifts(gifts)
+// function writeCards(arr, name){
+//   const empty =[]
+//   for(let i=0;i<arr.length; i++){
+//     const card = `Thank you, ${arr[i]}, for the wonderful ${name} gift!`
+//     empty.push(card)
+//   }
+//   return empty
+// }
+
+// console.log(writeCards(["Charlie", "Samip", "Ali"], "birthday"))
+
+
+// function countDown(num){
+//   let count = num
+//   while (count > 0){ 
+//     console.log(count--)
+// }
+// }
+// countDown(5)
